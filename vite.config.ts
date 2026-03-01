f13b8877613225e2deaa26f76f95467163488ea6
cerogab <gabegcordoba@gmail.com>
2026-03-01 10:06:20 -0500

    fix: split vendor chunk to eliminate build warnings

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,14 @@ export default defineConfig({
   build: {
     target: 'esnext',
     outDir: 'dist',
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router'],
+        },
+      },
+    },
   },
   server: {
     port: 3000,
