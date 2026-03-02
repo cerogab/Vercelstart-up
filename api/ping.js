496d9cccc215945f41d9dbd7a5c232e3296ca56e
cerogab <gabegcordoba@gmail.com>
2026-03-02 10:42:09 -0500

    chore: remove debug ping endpoint

--- api/ping.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    ok: true,
-    method: req.method,
-    hasResendKey: !!process.env.RESEND_API_KEY,
-    resendKeyLength: process.env.RESEND_API_KEY?.length ?? 0,
-    nodeVersion: process.version,
-  });
-}
